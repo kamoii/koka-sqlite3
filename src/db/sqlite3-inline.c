9cc19c994d526fff3b287b637fb35fef01e420e7
kamoii <>
2022-05-15 16:56:10 +0900

    Add some binding API

--- src/db/sqlite3-inline.c
+++ src/db/sqlite3-inline.c
@@ -1,11 +1,37 @@
 
 #include <sqlite3.h>
 
+// utility.
+// kklib/integer.h doesn't offer kk_integer_t -> int clamp function.
+static inline int kk_integer_clamp_int(kk_integer_t x, kk_context_t* ctx) {
+#if INT_MAX == INT64_MAX
+  return (int)kk_integer_clamp64(x,ctx);
+#elif INT_MAX == INT32_MAX
+  return (int)kk_integer_clamp32(x,ctx);
+#elif INT_MAX == INT16_MAX
+  return (int)kk_integer_clamp16(x,ctx);
+#else
+#error "can't decide platforms int size"
+#endif
+}
+
 // freeing must be done by the user
 static void kk_sqlite3_free_do_nothing( void* p, kk_block_t* b, kk_context_t* ctx ) {
   kk_unused(p);
   kk_unused(b);
   kk_unused(ctx);
+}
+
+static inline kk_std_core__error kk_sqlite3_read_error( int error_code, kk_context_t* ctx ) {
+  return kk_std_core__new_Error(kk_std_core__new_Exception(kk_string_alloc_from_qutf8(sqlite3_errstr(error_code), ctx), kk_std_core__new_ExnError(ctx), ctx), ctx);
+}
+
+static inline kk_std_core__error kk_sqlite3_unit_result( int result, kk_context_t* ctx ) {
+  if (result == SQLITE_OK) {
+    return kk_error_ok(kk_unit_box(kk_Unit), ctx);
+  } else {
+    return kk_sqlite3_read_error(result, ctx);
+  }
 }
 
 // * open
@@ -66,12 +92,39 @@ static kk_std_core__error kk_sqlite3_prepare_v2( kk_box_t db_, kk_string_t sql, 
     kk_box_t stmt_box = kk_cptr_raw_box(&kk_sqlite3_free_do_nothing, stmt, ctx);
     return kk_error_ok(stmt_box, ctx);
   } else {
-    kk_string_t msg;
-    msg = kk_string_alloc_from_qutf8(sqlite3_errstr(result), ctx);
-    return kk_std_core__new_Error(kk_std_core__new_Exception(msg, kk_std_core__new_ExnError(ctx), ctx), ctx);
+    return kk_sqlite3_read_error(result, ctx);
   }
 }
 
-// The calling procedure is responsible for deleting the compiled SQL statement
-// using sqlite3_finalize() after it has finished with it.
+// * Binding Values To Prepared Statements
+// https://www.sqlite.org/c3ref/bind_blob.html
+
+static kk_std_core__error kk_sqlite3_bind_null( kk_box_t stmt_, kk_integer_t index_, kk_context_t* ctx ) {
+  sqlite3_stmt *stmt = (sqlite3_stmt*)kk_cptr_raw_unbox(stmt_);
+  int index = kk_integer_clamp_int(index_, ctx);
+  return kk_sqlite3_unit_result(sqlite3_bind_null(stmt, index),ctx);
+}
+
+static kk_std_core__error kk_sqlite3_bind_int64( kk_box_t stmt_, kk_integer_t index_, int64_t value, kk_context_t* ctx ) {
+  sqlite3_stmt *stmt = (sqlite3_stmt*)kk_cptr_raw_unbox(stmt_);
+  int index = kk_integer_clamp_int(index_, ctx);
+  return kk_sqlite3_unit_result(sqlite3_bind_int64(stmt, index, (sqlite3_int64)value), ctx);
+}
+
+static kk_std_core__error kk_sqlite3_bind_text( kk_box_t stmt_, kk_integer_t index_, kk_string_t value_, kk_context_t* ctx ) {
+  sqlite3_stmt *stmt = (sqlite3_stmt*)kk_cptr_raw_unbox(stmt_);
+  int index = kk_integer_clamp_int(index_, ctx);
+  kk_ssize_t len;
+  const char *value = kk_string_cbuf_borrow(value_, &len);
+  // QUESTION: Is (int)len safe ? Don't know about kk_ssize_t
+  // TODO: SQLITE_TRANSIENT means we copy string content every time.
+  return kk_sqlite3_unit_result(sqlite3_bind_text(stmt, index, value, (int)len, SQLITE_TRANSIENT), ctx);
+}
+
+// * finalizer
 // https://www.sqlite.org/c3ref/finalize.html
+
+static kk_std_core__error kk_sqlite3_finalize( kk_box_t stmt_, kk_context_t* ctx ) {
+  sqlite3_stmt *stmt = (sqlite3_stmt*)kk_cptr_raw_unbox(stmt_);
+  return kk_sqlite3_unit_result(sqlite3_finalize(stmt), ctx);
+}
